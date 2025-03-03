3183a0c5b9f4ae70dda094a22ec3748d9dad257e
Kuan <chenkuansk@ad.garmin.com>
2025-03-03 22:21:34 +0800

    empty project

---
+++ src/main/java/com/example/freelance_resource_backend/FreelanceResourceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.freelance_resource_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceResourceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceResourceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/freelance_resource_backend/FreelanceResourceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freelance_resource_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceResourceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
