de883b1efd38362382de821c0cfd41182b126870
Kuan <chenkuansk@ad.garmin.com>
2025-03-04 09:17:32 +0800

    add CourseRating enum

---
+++ src/main/java/com/example/freelance_resource_backend/enums/CourseRating.java
@@ -0,0 +1,28 @@
+package com.example.freelance_resource_backend.enums;
+
+public enum CourseRating {
+	POOR(1),
+	FAIR(2),
+	GOOD(3),
+	EXCELLENT(4);
+
+	private final int value;
+
+	CourseRating(int value) {
+		this.value = value;
+	}
+
+	public static CourseRating getValue(int value) {
+		for (CourseRating type : CourseRating.values()) {
+			if (type.getValue() == value) {
+				return type;
+			}
+		}
+
+		throw new IllegalArgumentException("Unknown enum value: %s. Valid values are [1, 2, 3, 4, 5]".formatted(value));
+	}
+
+	public int getValue() {
+		return value;
+	}
+}
