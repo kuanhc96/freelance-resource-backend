1b23d65081c60c08a1b1198038730c814de54808
Kuan <chenkuansk@ad.garmin.com>
2025-08-08 10:08:21 +0800

    add test controller and dto to get configs

--- src/main/java/com/example/freelance_resource_backend/FreelanceResourceBackendApplication.java
+++ src/main/java/com/example/freelance_resource_backend/FreelanceResourceBackendApplication.java
@@ -2,10 +2,14 @@ package com.example.freelance_resource_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import com.example.freelance_resource_backend.dto.TestAccountInfoDto;
 
 @SpringBootApplication
 @EnableWebSecurity(debug=true)
+@EnableConfigurationProperties(value={TestAccountInfoDto.class})
 public class FreelanceResourceBackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/freelance_resource_backend/controller/AppInfoController.java
@@ -0,0 +1,41 @@
+package com.example.freelance_resource_backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import com.example.freelance_resource_backend.dto.TestAccountInfoDto;
+
+@RestController
+@RequestMapping("/appInfo")
+@RequiredArgsConstructor
+public class AppInfoController {
+	@Value("${app.version}")
+	private String version;
+
+	private final Environment env;
+
+	private final TestAccountInfoDto testAccountInfoDto;
+
+	@GetMapping("/version")
+	public ResponseEntity<String> getAppVersion() {
+		return ResponseEntity.ok(version);
+	}
+
+	@GetMapping("/javaVersion")
+	public ResponseEntity<String> getJavaVersion() {
+		return ResponseEntity.ok(env.getProperty("JAVA_HOME"));
+	}
+
+	@GetMapping("/testAccount")
+	public ResponseEntity<TestAccountInfoDto> getTestAccountInfo() {
+		return ResponseEntity.ok(testAccountInfoDto);
+	}
+
+}
