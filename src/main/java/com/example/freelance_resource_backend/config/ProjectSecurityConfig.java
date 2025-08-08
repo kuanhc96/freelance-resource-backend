b128e7aaff1b664077422b4b5a61ea2a09f3e010
Kuan <chenkuansk@ad.garmin.com>
2025-08-08 11:28:36 +0800

    project security config should be disabled for dev profile

--- src/main/java/com/example/freelance_resource_backend/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/freelance_resource_backend/config/ProjectSecurityConfig.java
@@ -5,6 +5,9 @@ import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,6 +30,7 @@ import com.example.freelance_resource_backend.handler.CustomAuthenticationFailur
 import com.example.freelance_resource_backend.handler.CustomAuthenticationSuccessHandler;
 
 @Configuration
+@Profile("!dev")
 public class ProjectSecurityConfig {
 	@Autowired
 	private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
