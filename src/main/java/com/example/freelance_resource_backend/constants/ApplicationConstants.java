0d6bb748b24feb01311d7ea6c180517cd6c2b5ef
Kuan <chenkuansk@ad.garmin.com>
2025-08-13 12:12:31 +0800

    add authorization constants

--- src/main/java/com/example/freelance_resource_backend/constants/ApplicationConstants.java
+++ src/main/java/com/example/freelance_resource_backend/constants/ApplicationConstants.java
@@ -9,4 +9,8 @@ public class ApplicationConstants {
 	// client level security
 	public static final String ROLE_INTEGRATION_TEST = "hasRole('ROLE_INTEGRATION_TEST')";
 	public static final String ROLE_FREELANCE_FE = "hasRole('ROLE_FREELANCE_FE')";
+
+	public static final String INSTRUCTOR = "(" + ROLE_INSTRUCTOR + " and " + ROLE_FREELANCE_FE + ") or " + ROLE_INTEGRATION_TEST;
+	public static final String STUDENT = "(" + ROLE_STUDENT + " and " + ROLE_FREELANCE_FE + ") or " + ROLE_INTEGRATION_TEST;
+	public static final String INSTRUCTOR_OR_STUDENT = ROLE_INSTRUCTOR + " or " + ROLE_STUDENT + " or " + ROLE_INTEGRATION_TEST;
 }
