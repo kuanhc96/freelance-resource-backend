69092b351164c6eb5f62bff88f0329227054d7d9
Kuan <chenkuansk@ad.garmin.com>
2025-09-16 16:10:10 +0800

    setup test property files

--- src/test/java/com/example/freelance_resource_backend/controller/integration/helper/OAuthTokenHelper.java
+++ src/test/java/com/example/freelance_resource_backend/controller/integration/helper/OAuthTokenHelper.java
@@ -12,12 +12,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 
 @Component
+@TestPropertySource("/test-${spring.profiles.active:dev}.properties")
 public class OAuthTokenHelper {
 
 	private String accessToken;
@@ -25,32 +27,37 @@ public class OAuthTokenHelper {
 	public OAuthTokenHelper(
 		@Value("${clientId}") String clientId,
 		@Value("${clientSecret}") String clientSecret,
-		@Value("${authServerLocation}") String authServerLocation,
-		@Value("${accessTokenUrl}") String accessTokenUrl
+		@Value("${application.authUrl}") String authServerLocation,
+		@Value("${application.tokenEndpoint}") String accessTokenUrl,
+		@Value("${spring.profiles.active}") String profile
 	) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-		headers.setBasicAuth(clientId, clientSecret, StandardCharsets.UTF_8);
+		if (profile.equals("dev")) {
+			accessToken = "";
+		} else {
+			RestTemplate restTemplate = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+			headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+			headers.setBasicAuth(clientId, clientSecret, StandardCharsets.UTF_8);
 
-		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-		params.add("grant_type", "client_credentials");
-		params.add("scope", "INTEGRATION_TEST");
+			MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+			params.add("grant_type", "client_credentials");
+			params.add("scope", "INTEGRATION_TEST");
 
-		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(params, headers);
-		ResponseEntity<Map> response = restTemplate.exchange(
-				authServerLocation + accessTokenUrl,
-				HttpMethod.POST,
-				request,
-				Map.class
-		);
+			HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(params, headers);
+			ResponseEntity<Map> response = restTemplate.exchange(
+					authServerLocation + accessTokenUrl,
+					HttpMethod.POST,
+					request,
+					Map.class
+			);
 
-		if (response.getStatusCode() != HttpStatus.OK || response.getBody() == null) {
-			throw new IllegalStateException("Token request failed: " + response.getStatusCode());
+			if (response.getStatusCode() != HttpStatus.OK || response.getBody() == null) {
+				throw new IllegalStateException("Token request failed: " + response.getStatusCode());
+			}
+
+			accessToken = response.getBody().get("access_token").toString();
 		}
-
-		accessToken = response.getBody().get("access_token").toString();
 	}
 
 	public HttpHeaders getHeadersWithAccessToken() {
--- src/test/java/com/example/freelance_resource_backend/controller/integration/service/UserControllerIT.java
+++ src/test/java/com/example/freelance_resource_backend/controller/integration/service/UserControllerIT.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.util.MultiValueMap;
@@ -15,9 +16,11 @@ import org.springframework.web.client.RestTemplate;
 
 import com.example.freelance_resource_backend.controller.integration.config.TestConfig;
 import com.example.freelance_resource_backend.controller.integration.helper.OAuthTokenHelper;
+import com.example.freelance_resource_backend.dto.response.user.GetUserResponse;
 
 @ExtendWith(SpringExtension.class)
 @SpringJUnitConfig(classes = {TestConfig.class, OAuthTokenHelper.class})
+@TestPropertySource("/test-test.properties")
 public class UserControllerIT {
 	@Autowired
 	private OAuthTokenHelper oAuthTokenHelper;
@@ -39,5 +42,18 @@ public class UserControllerIT {
 			String.class
 		).getBody();
 		assertNotNull(result);
+	}
+
+	@Test
+	void getUserByUserGUID_success() {
+		String userGUID = "36946828-6696-4b83-be33-6093f2efef70";
+		GetUserResponse user = restTemplate.exchange(
+			"http://localhost:8081/user/" + userGUID,
+			HttpMethod.GET,
+			httpEntity,
+			GetUserResponse.class
+		).getBody();
+
+		assertNotNull(user);
 	}
 }
