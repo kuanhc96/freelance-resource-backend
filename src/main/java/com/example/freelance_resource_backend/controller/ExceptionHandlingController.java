84d8370c5948199ef6350e4959372a46a5f63d39
Kuan <chenkuansk@ad.garmin.com>
2025-09-18 14:47:03 +0800

    add controller advice for error handling

---
+++ src/main/java/com/example/freelance_resource_backend/controller/ExceptionHandlingController.java
@@ -0,0 +1,17 @@
+package com.example.freelance_resource_backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.example.freelance_resource_backend.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ExceptionHandlingController extends ResponseEntityExceptionHandler {
+	@ExceptionHandler({ResourceNotFoundException.class})
+	ResponseEntity<String> handleBusinessException_NotFound(Exception ex) {
+		return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+	}
+}
